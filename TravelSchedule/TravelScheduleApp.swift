1c209f5e6ef8768c7a267a13ffcb4c3e4dae96c8
Azonaz <malinina.a@gmail.com>
2024-03-08 16:25:35 +0200

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Админ on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
