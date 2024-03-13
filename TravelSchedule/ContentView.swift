a8cda275f53dfb85b5117ccc99d208446ddd5c9a
Azonaz <malinina.a@gmail.com>
2024-03-13 09:47:12 +0200

    Завершение задачи 18 спринта: - полностью сформирован openapi.yaml - добавлены все 8 сетевых сервисов API - реализованы вызовы сервисов

--- TravelSchedule/ContentView.swift
+++ TravelSchedule/ContentView.swift
@@ -1,11 +1,5 @@
-//
-//  ContentView.swift
-//  TravelSchedule
-//
-//  Created by Админ on 08/03/2024.
-//
-
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
@@ -16,6 +10,152 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            search()
+            schedule()
+            thread()
+            stations()
+            settlement()
+            carrier()
+            stationsList()
+            copiright()
+        }
+    }
+    
+    //Расписание рейсов между станциями
+    func search() {
+        let client = Client(serverURL: try! Servers.server1(),
+                            transport: URLSessionTransport())
+        let service = SearchRoutesService(client: client,
+                                          apikey: apiKey)
+        Task {
+            do {
+                let routes = try await service.searchRoutes(from: "c146",
+                                                            to: "c213",
+                                                            date: "2024-03-12")
+                print(routes)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    //Расписание рейсов по станции
+    func schedule() {
+        let client = Client(serverURL: try! Servers.server1(),
+                            transport: URLSessionTransport())
+        let service = ScheduleService(client: client,
+                                      apikey: apiKey)
+        Task {
+            do {
+                let schedule = try await service.getSchedule(station: "s9600213",
+                                                             date: "2024-03-12")
+                print(schedule)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    //Список станций следования
+    func thread() {
+        let client = Client(serverURL: try! Servers.server1(),
+                            transport: URLSessionTransport())
+        let service = ThreadService(client: client,
+                                    apikey: apiKey)
+        Task {
+            do {
+                let thread = try await service.getThread(uid: "176YE_7_2")
+                print(thread)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    //Список ближайших станций
+    func stations() {
+        let client = Client(serverURL: try! Servers.server1(),
+                            transport: URLSessionTransport())
+        let service = NearestStationsService(client: client,
+                                             apikey: apiKey)
+        Task {
+            do {
+                let stations = try await service.getNearestStations(lat: 59.864177,
+                                                                    lng: 30.319163,
+                                                                    distance: 50)
+                print(stations)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    //Ближайший город
+    func settlement() {
+        let client = Client(serverURL: try! Servers.server1(),
+                            transport: URLSessionTransport())
+        let service = NearestSettlementService(client: client,
+                                               apikey: apiKey)
+        Task {
+            do {
+                let settlement = try await service.getNearestSettlement(lat: 59.864177,
+                                                                        lng: 30.319163)
+                print(settlement)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    //Информация о перевозчике
+    func carrier() {
+        let client = Client(serverURL: try! Servers.server1(),
+                            transport: URLSessionTransport())
+        let service = CarriersService(client: client,
+                                      apikey: apiKey)
+        Task {
+            do {
+                let carriers = try await service.getCarriers(code: "680")
+                print(carriers)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    //Список всех доступных станций
+    func stationsList() {
+        let client = Client(serverURL: try! Servers.server1(),
+                            transport: URLSessionTransport())
+        let service = StationsListService(client: client,
+                                          apikey: apiKey)
+        Task {
+            do {
+                let stations = try await service.getStationsList()
+                let decodeStations = try await Data(collecting: stations, upTo: 50*1024*1024)
+                let starionsList = try JSONDecoder().decode(StationsList.self, from: decodeStations)
+                print(starionsList)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    //Копирайт Яндекс Расписаний
+    func copiright() {
+        let client = Client(serverURL: try! Servers.server1(),
+                            transport: URLSessionTransport())
+        let service = CopyrightService(client: client,
+                                       apikey: apiKey)
+        Task {
+            do {
+                let copiright = try await service.getCopyright()
+                print(copiright)
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
---
+++ TravelSchedule/Helpers/Constants.swift
@@ -0,0 +1,3 @@
+import Foundation
+
+let apiKey = "869ea271-1d3a-4c58-a048-a8dafbf0235c"
