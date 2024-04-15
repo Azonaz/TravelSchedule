19eab9b0e841f116adc598223bd02bd6c9bc057f
Azonaz <malinina.a@gmail.com>
2024-04-15 16:32:02 +0300

    подготовлен главный экран (моковые стори, блок выбора направлений)

--- TravelSchedule/Helpers/Fonts.swift
+++ TravelSchedule/Helpers/Fonts.swift
@@ -7,5 +7,6 @@ extension Font {
     static let regular17 = Font.system(size: 17, weight: .regular)
 
     // Bold
+    static let bold17 = Font.system(size: 17, weight: .bold)
     static let bold24 = Font.system(size: 24, weight: .bold)
 }
--- TravelSchedule/View/ContentView.swift
+++ TravelSchedule/View/ContentView.swift
@@ -4,6 +4,100 @@ import OpenAPIURLSession
 struct ContentView: View {
     var body: some View {
         StoryPreView()
+        ZStack {
+            BlueRectangle()
+            HStack {
+                WhiteRectangle()
+                Spacer()
+                ReplaceCircle()
+            }
+        }
+        FindButton()
+    }
+}
+
+struct BlueRectangle: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 16)
+            .fill(Color.blueUniversal)
+            .frame(height: 128)
+            .padding(.horizontal, 16)
+    }
+}
+
+struct WhiteRectangle: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 16)
+            .fill(Color.white)
+            .frame(height: 96)
+            .overlay(
+                VStack(alignment: .leading, spacing: 28) {
+                    FromButton()
+                    ToButton()
+                }
+            )
+            .padding(.leading, 32)
+    }
+}
+
+struct ReplaceCircle: View {
+    var body: some View {
+        Circle()
+            .fill(Color.white)
+            .frame(width: 36)
+            .overlay(
+                Image(.change)
+                    .foregroundStyle(Color.blueUniversal)
+                    .onTapGesture {
+                    }
+            )
+            .padding(.trailing, 32)
+    }
+}
+
+struct FromButton: View {
+    var body: some View {
+        Button(action: {
+
+        }, label: {
+            Text("Откуда")
+        })
+        .foregroundColor(.grayUniversal)
+        .font(.regular17)
+        .lineLimit(1)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding([.leading, .trailing], 16)
+    }
+}
+
+struct ToButton: View {
+    var body: some View {
+        Button(action: {
+
+        }, label: {
+            Text("Куда")
+        })
+        .foregroundColor(.grayUniversal)
+        .font(.regular17)
+        .lineLimit(1)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding([.leading, .trailing], 16)
+    }
+}
+
+struct FindButton: View {
+    var body: some View {
+        Button(action: {
+
+        }, label: {
+            Text("Найти")
+                .foregroundColor(.white)
+                .font(.bold17)
+        })
+        .frame(width: 150, height: 60)
+        .background(.blueUniversal)
+        .cornerRadius(16)
+        .padding(.vertical, 8)
     }
 }
 
