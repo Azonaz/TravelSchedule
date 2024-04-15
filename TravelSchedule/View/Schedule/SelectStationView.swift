dc4d9fe7d62da0a418a13afb45127f8174688f7c
Azonaz <malinina.a@gmail.com>
2024-04-15 17:37:06 +0300

    чистка кода

---
+++ TravelSchedule/View/Schedule/SelectStationView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct SelectStationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectStationView()
+}
