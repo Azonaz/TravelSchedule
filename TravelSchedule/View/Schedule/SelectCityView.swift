c8e4fad81ba6e1bb2aa38090e7c1b43e0ec850cd
Azonaz <malinina.a@gmail.com>
2024-04-23 11:18:02 +0300

    коррекция показа надписи, если не найден город/станция

--- TravelSchedule/View/Schedule/SelectCityView.swift
+++ TravelSchedule/View/Schedule/SelectCityView.swift
@@ -20,29 +20,36 @@ struct SelectCityView: View {
         let filteredCities = viewModel.cities.filter { city in
             searchText.isEmpty || city.name.localizedCaseInsensitiveContains(searchText)
         }
-        return filteredCities.isEmpty ?
-        AnyView(Text(Constants.cityNotFound)
-            .foregroundColor(.blackDay)
-            .font(.bold24)
-            .padding()) :
-        AnyView(
-            ScrollView {
-                VStack(spacing: 0) {
-                    ForEach(filteredCities, id: \.name) { city in
-                        NavigationLink(destination: SelectStationView(city: city, selectionType: selectionType)) {
-                            HStack {
-                                Text(city.name)
-                                    .font(.regular17)
-                                Spacer()
-                                Image(systemName: "chevron.right")
-                                    .imageScale(.large)
+        return AnyView(
+            ZStack {
+                if filteredCities.isEmpty {
+                    VStack {
+                        Text(Constants.cityNotFound)
+                            .foregroundColor(.blackDay)
+                            .font(.bold24)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    }
+                } else {
+                    ScrollView {
+                        VStack(spacing: 0) {
+                            ForEach(filteredCities, id: \.name) { city in
+                                NavigationLink(destination: SelectStationView(city: city,
+                                                                              selectionType: selectionType)) {
+                                    HStack {
+                                        Text(city.name)
+                                            .font(.regular17)
+                                        Spacer()
+                                        Image(systemName: "chevron.right")
+                                            .imageScale(.large)
+                                    }
+                                    .contentShape(Rectangle())
+                                    .frame(height: 60)
+                                    .padding(.horizontal, 16)
+                                }
+                                .foregroundColor(.blackDay)
+                                .buttonStyle(PlainButtonStyle())
                             }
-                            .contentShape(Rectangle())
-                            .frame(height: 60)
-                            .padding(.horizontal, 16)
                         }
-                        .foregroundColor(.blackDay)
-                        .buttonStyle(PlainButtonStyle())
                     }
                 }
             }
--- TravelSchedule/View/Schedule/SelectStationView.swift
+++ TravelSchedule/View/Schedule/SelectStationView.swift
@@ -32,34 +32,38 @@ struct SelectStationView: View {
         let filteredStations = city.stations.filter { station in
             searchText.isEmpty || station.localizedCaseInsensitiveContains(searchText)
         }
-        return filteredStations.isEmpty ?
-        AnyView(
-            Text(Constants.stationNotFound)
-                .foregroundColor(.blackDay)
-                .font(.bold24)
-                .padding()
-        ) :
-        AnyView(
-            ScrollView {
-                VStack(spacing: 0) {
-                    ForEach(filteredStations, id: \.self) { station in
-                        NavigationLink(destination: TabBarView(), isActive: $isNavigationActive) {
-                            HStack {
-                                Text(station)
-                                    .font(.regular17)
-                                Spacer()
-                                Image(systemName: "chevron.right")
-                                    .imageScale(.large)
-                            }
-                            .contentShape(Rectangle())
-                            .frame(height: 60)
-                            .padding(.horizontal, 16)
-                            .onTapGesture {
-                                handleStationSelection(station: station)
+        return AnyView(
+            ZStack {
+                if filteredStations.isEmpty {
+                    VStack {
+                        Text(Constants.stationNotFound)
+                            .foregroundColor(.blackDay)
+                            .font(.bold24)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    }
+                } else {
+                    ScrollView {
+                        VStack(spacing: 0) {
+                            ForEach(filteredStations, id: \.self) { station in
+                                NavigationLink(destination: TabBarView(), isActive: $isNavigationActive) {
+                                    HStack {
+                                        Text(station)
+                                            .font(.regular17)
+                                        Spacer()
+                                        Image(systemName: "chevron.right")
+                                            .imageScale(.large)
+                                    }
+                                    .contentShape(Rectangle())
+                                    .frame(height: 60)
+                                    .padding(.horizontal, 16)
+                                    .onTapGesture {
+                                        handleStationSelection(station: station)
+                                    }
+                                }
+                                .foregroundColor(.blackDay)
+                                .buttonStyle(PlainButtonStyle())
                             }
                         }
-                        .foregroundColor(.blackDay)
-                        .buttonStyle(PlainButtonStyle())
                     }
                 }
             }
